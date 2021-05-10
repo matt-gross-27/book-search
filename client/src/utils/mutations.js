9de4fa8ba633bbc14a08fd4352ffbf11c3b96144
Matt Gross <mbgross111@gmail.com>
2021-05-10 02:16:29 -0700

    set up Apollo Provider, front end queries.js and mutations.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,58 @@
+import gql from 'graphql-tag';
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $password: String!, $email: String!) {
+    addUser(username: $username, password: $password, email: $email) {
+      token
+      user {
+        _id
+  			username
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id,
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: BookInput!) {
+  saveBook(input: $input) {
+    username
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+}
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+  removeBook(bookId: $bookId) {
+    username
+    savedBooks {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+}
+`;
